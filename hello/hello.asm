392648561eee7e70e1d8b96441f4a63e68b8ff6d
Janos Jarecsni <janos.jarecsni@gmail.com>
2023-10-26 20:01:03 +0100

    tidy up

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,4 +1,8 @@
 global start
+section .data
+  msg:    db      "Hello, world!", 10
+  .len:   equ     $ - msg
+
 section .text
 start:
   mov rax, 0x2000004 ; write
@@ -9,6 +13,3 @@ start:
   mov rax, 0x2000001 ; exit
   mov rdi, 0
   syscall
-section .data
-msg:    db      "Hello, world!", 10
-.len:   equ     $ - msg
