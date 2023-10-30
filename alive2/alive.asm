c84164bdec3332d194b9f634cb21c67df59de814
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-10-30 23:02:17 +0000

    printing numbers with printf

---
+++ alive2/alive.asm
@@ -0,0 +1,52 @@
+;alive.asm
+
+section .data
+    msg1        db  "Hello, World!", 10, 0
+    msg1Len     equ $-msg1-1                    ; measure length, take away 1 (for the 0 byte at the end)
+    msg2        db  "Alive and kicking!", 10, 0
+    msg2Len     equ $-msg2-1                  
+    radius      dq  357
+    pi          dq  3.14
+    fmtstr      db  "%s", 10, 0
+    fmtflt      db  "%1f", 10, 0
+    fmtint      db  "%d", 10, 0
+
+section .bss
+
+section .text
+    extern printf
+    global main
+
+main:
+    push    rbp                     ; function prolog
+    mov     rbp, rsp                ; -"-
+    ; print msg1
+    mov     rax, 0
+    mov     rdi, fmtstr
+    mov     rsi, msg1
+    call printf
+
+    ; print msg2
+    mov     rax, 0
+    mov     rdi, fmtstr
+    mov     rsi, msg2
+    call printf
+
+    ; print radius
+    mov     rax, 0
+    mov     rdi, fmtint
+    mov     rsi, radius
+    call printf
+
+    ; print pi
+    mov     rax, 1                  ; floating point involved (xmm register used)
+    movq    xmm0, [pi]
+    mov     rdi, fmtflt
+    call printf
+
+    mov     rsp, rbp                ; function epilogue
+    pop     rbp 
+    mov     rax, 60
+    mov     rdi, 0
+    syscall
+
